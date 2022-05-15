e443ade340bf0cbc0eeea0136236c10a17a4f73d
nikolastas-laptop <nikolas2000tas@gmail.com>
2022-05-15 16:10:58 +0300

    auth code
    monolith arch

---
+++ server.js
@@ -0,0 +1,66 @@
+const express =  require('express');
+const handler = require('./login_handling/login.js');
+const jwt = require("jsonwebtoken");
+app = express();
+
+app.get("/",(req,res) => {
+    res.status(200).sendFile(__dirname + "/index.html");
+})
+
+
+app.get("/login", (req, res) => {
+    
+    res.status(200).sendFile(__dirname + "/login_handling/login.html");
+}
+)
+
+app.get("/home",(req,res)=>{
+    const token = req.query.token;
+    // decode token with jwt
+    // if (token) {
+    //     // verifies secret and checks exp
+    //     jwt.verify(token, "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", (err, decoded) => {
+    //         if (err) {
+    //             return res.status(401).json({
+    //                 message: "Unauthorized, wrong token"
+    //             });
+    //         } else {
+    //             // if everything is good, save to request for use in other routes
+    //             req.decoded = decoded;
+    //             res.status(200).sendFile(__dirname + "/home.html");
+    //         }
+    //     });
+    // } else {
+    //     // if there is no token
+    //     // return an error
+    //     return res.status(401).send({
+    //         message: "Unauthorized"
+    //     });
+    // }
+    function parseJwt (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
+    };
+    
+    if(token){
+        let decodeToken = parseJwt(token);
+        console.log(decodeToken);
+        // SQL QUERY TO GET ALL SUBSCRIPTION
+        let query = `SELECT SubcriptionType FROM subscription WHERE username = '${decodeToken.sub}'`;
+        res.send(`Hello ${decodeToken.name}`);
+    }
+    else {
+        res.status(401).send("Unauthorized user");
+    }
+
+});
+
+var port = 6660 ;
+app.listen(process.env.port || port, () => {
+    console.log(`Server is running on port ${port}`);
+});
