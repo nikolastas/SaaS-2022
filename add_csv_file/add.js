2da30e662b0ca2e7c9a0ef593ac6b9319d773a92
nikolastas-laptop <nikolas2000tas@gmail.com>
2022-05-16 20:07:25 +0300

    some file changes

---
+++ add_csv_file/add.js
@@ -0,0 +1,11 @@
+module.exports.upload_csv = async (req, res) => {
+
+    if(req.params.filename ){
+        
+    }
+    else{
+        //send res error need parameters
+        res.send("error with parameters");
+    }
+    
+}
