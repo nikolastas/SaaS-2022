53c16a2dd17b9ba9b61b7de51a09f3b28b38f3c0
nikolastas-laptop <nikolas2000tas@gmail.com>
2022-05-16 19:46:15 +0300

    add files

--- database/add.py
+++ database/add.py
@@ -11,23 +11,45 @@ def read_file(filename):
     file.close()
     # return data
     return data
+
 data = read_file('credentials.txt').split()
+# get data from file
+
 username =data[0]
 password = data[1]
 database = data[2]
+
 # connect to database with username and password using mysql connector
-cnx = mysql.connector.connect(user=username, password=password, database=database)
+try:
+    cnx = mysql.connector.connect(user=username, password=password, database=database)
+except:
+    print("error with credentials")
+print("Database connected")
+
 # create cursor
 cursor = cnx.cursor()
-# SELECT ALL SUBSCRIPTIONS 
-query = ("SELECT * FROM subscriptions")
-cursor.execute(query)
-# fetch all rows
-rows = cursor.fetchall()
-# print all rows
-for row in rows:
-    print(row)
-# close cursor
-cursor.close()
-# test database connection
-print('Connected to database')
+
+def add_data(data, database):
+    # if data is list make a for loop
+    if (type(data) == tuple):
+        query = ("INSERT INTO "+ database+" VALUES "+str(data) )
+        cursor.execute(query)
+        # fetch all rows
+        rows = cursor.fetchall()
+        cursor.close()
+        # insert data in database
+        cnx.commit()
+        # close connection
+        cnx.close()
+        return 
+    for d in data:
+        # make the data query 
+        query = ("INSERT INTO"+ database+" VALUES "+str(d) )
+        cursor.execute(query)
+        # fetch all rows
+        rows = cursor.fetchall()
+    cnx.commit()
+    cnx.close()
+    return
+
+add_data(("nikos3", '2020-10-10', '2022-12-30', "VALID"), "subscriptions")
