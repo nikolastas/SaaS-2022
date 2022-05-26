586670105af7bbb6a94b6860e60d7ac335884d6d
nikolastas-laptop <nikolas2000tas@gmail.com>
2022-05-26 21:04:02 +0300

    SQL files updated for database

--- database/generate database.sql
+++ database/generate database.sql
@@ -2,34 +2,34 @@ CREATE TABLE ActualTotalLoad (Datetime datetime NOT NULL,
 ResolutionCode varchar(255), 
 TotalLoadValue float, 
 UpdateTime datetime NULL, 
-AreaAreaName varchar(255) NOT NULL, 
-PRIMARY KEY (Datetime, AreaAreaName));
+MapCode varchar(255) NOT NULL,
+PRIMARY KEY (Datetime, MapCode));
 CREATE TABLE AggrGenerationPerType (DateTime datetime NOT NULL, 
 ResolutionCode varchar(255), 
 ProductionType varchar(255) NOT NULL, 
 ActualGenerationOutput float, 
 ActualConsumption float, 
 UpdateTime datetime NULL, 
-AreaAreaName varchar(255) NOT NULL, 
-PRIMARY KEY (DateTime, ProductionType, AreaAreaName));
+MapCode varchar(255) NOT NULL,
+PRIMARY KEY (DateTime, ProductionType, MapCode));
 CREATE TABLE Area (AreaTypeCode varchar(255) NOT NULL, 
 AreaName varchar(255) NOT NULL, 
 MapCode varchar(255) NOT NULL, 
-PRIMARY KEY (AreaName));
+PRIMARY KEY (MapCode));
 CREATE TABLE PhysicalFlows (DateTime datetime NOT NULL,
 ResolutionCode varchar(255), 
 FlowValue float, 
 UpdateTime datetime NULL,
-InAreaAreaName varchar(255) NOT NULL, 
-OutAreaAreaName varchar(255) NOT NULL,
+InMapCode varchar(255) NOT NULL,
+OutMapCode varchar(255) NOT NULL,
  
-PRIMARY KEY (DateTime, InAreaAreaName, OutAreaAreaName));
+PRIMARY KEY (DateTime, InMapCode, OutMapCode));
 CREATE TABLE Subscriptions (Username varchar(255) NOT NULL, 
 SubscriptionStartTime datetime NULL, 
 SubscriptionEndTime datetime NULL, 
 SubcriptionType varchar(255), 
 PRIMARY KEY (Username));
-ALTER TABLE ActualTotalLoad ADD CONSTRAINT FKActualTota412327 FOREIGN KEY (AreaAreaName) REFERENCES Area (AreaName);
-ALTER TABLE AggrGenerationPerType ADD CONSTRAINT FKAggrGenera687323 FOREIGN KEY (AreaAreaName) REFERENCES Area (AreaName);
-ALTER TABLE PhysicalFlows ADD CONSTRAINT FKPhysicalFl871357 FOREIGN KEY (InAreaAreaName) REFERENCES Area (AreaName);
-ALTER TABLE PhysicalFlows ADD CONSTRAINT FKPhysicalFl555898 FOREIGN KEY (OutAreaAreaName) REFERENCES Area (AreaName);
+ALTER TABLE ActualTotalLoad ADD CONSTRAINT FKActualTota412327 FOREIGN KEY (MapCode) REFERENCES Area (MapCode);
+ALTER TABLE AggrGenerationPerType ADD CONSTRAINT FKAggrGenera687323 FOREIGN KEY (MapCode) REFERENCES Area (MapCode);
+ALTER TABLE PhysicalFlows ADD CONSTRAINT FKPhysicalFl871357 FOREIGN KEY (InMapCode) REFERENCES Area (MapCode);
+ALTER TABLE PhysicalFlows ADD CONSTRAINT FKPhysicalFl555898 FOREIGN KEY (OutMapCode) REFERENCES Area (MapCode);
