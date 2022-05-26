a2688bf7aebd937668066fde347d19d549486d21
nikolastas-laptop <nikolas2000tas@gmail.com>
2022-05-26 22:11:23 +0300

    some fixes

--- add_csv_file/add.js
+++ add_csv_file/add.js
@@ -35,6 +35,34 @@ function FillEmptyString (str, stuffing) {
     return str === ''? stuffing : str;
 }
 
+function return_correct_data(csv_json, folder){
+    let sql_query;
+    if(folder == "aggrgenerationpertype" || folder == "actualtotalload" ){
+        
+        sql_query = "INSERT INTO "+ folder +" VALUES"
+        for (let i = 0; i < csv_json.length - 1; i++) {
+            let temp = "('" + csv_json[i]['DateTime'] + "','" + csv_json[i]['ResolutionCode'] +"','"+ csv_json[i]['TotalLoadValue'] + "','" + csv_json[i]['UpdateTime'] + "','" + csv_json[i]['MapCode'] + "')" 
+            if(i < csv_json.length - 2)
+                temp += ",";
+            sql_query += temp;
+        }
+        sql_query += ";";
+        sql_query = sql_query.replaceAll("''", null);
+    }
+    else if( folder == "physicalflows"){
+        sql_query = "INSERT INTO "+ folder+" VALUES"
+        for (let i = 0; i < csv_json.length - 1; i++) {
+            let temp = "('" + csv_json[i]['DateTime'] + "','" + csv_json[i]['ResolutionCode'] +"','"+ csv_json[i]['FlowValue'] + "','"  + csv_json[i]['UpdateTime'] + "','" + csv_json[i]['InMapCode']  + "','" + csv_json[i]['OutMapCode']+ "')" 
+            if(i < csv_json.length - 2)
+                temp += ",";
+            sql_query += temp;
+        }
+        sql_query += ";";
+        sql_query = sql_query.replaceAll("''", null);
+    }
+    return sql_query;
+}
+
 module.exports.upload_csv = async (req, res) => {
     let csvData = [];
     if(req.params.filename && req.params.foldername){
@@ -48,59 +76,42 @@ module.exports.upload_csv = async (req, res) => {
         
 
         let csv_original;
-        if(text_last !== '') {
-            await compare_csv(filePath, text_last, "./data/"+folder+"/difference.csv");
-            csv_original = fs.readFileSync("./data/"+folder+"/difference.csv", 'utf8');
+        if(text_last != '') {
+            try{
+                await compare_csv(filePath, text_last, "./data/"+folder+"/difference.csv");
+                csv_original = fs.createReadStream("./data/"+folder+"/difference.csv", 'utf8');
             // delete not recent file
             // fs.unlinkSync(text_last);
+            }
+            catch{
+                res.status(400).send("Error1: File not found");
+                return;
+            }
         } 
         else
-            csv_original = fs.readFileSync(filePath, 'utf8');
-        
+            try{
+                csv_original = fs.readFileSync(filePath, 'utf8');
+            }
+            catch{
+                res.status(400).send("Error2: File not found");
+            }
+            
+        try{
         csv_json = CsvToJson(csv_original, '\t'); 
 
 
         // write latest input file in the text
         fs.writeFileSync("./add_csv_file/"+ folder + "_last.txt", filePath)
 
+        // make query
+        let sql_query = return_correct_data(csv_json, folder);
+        
 
-        let sql_query;
-        if(folder == "aggrgenerationpertype"){     
-            sql_query = "INSERT INTO aggrgenerationpertype () VALUES"
-            for (let i = 0; i < csv_json.length - 1; i++) {
-                let temp = "('" + csv_json[i]['DateTime'] + "','" + csv_json[i]['ResolutionCode'] +"','"+ csv_json[i]['ProductionType'] + "','" + csv_json[i]['ActualGenerationOutput'] + "','" + csv_json[i]['ActualConsumption']+ "','" + csv_json[i]['UpdateTime'] + "','" + csv_json[i]['AreaName'] + "')" 
-                if(i < csv_json.length - 2)
-                    temp += ",";
-                sql_query += temp;
-            }
-            sql_query += ";";
-            sql_query = sql_query.replaceAll("''", null);
+        res.status(200).send(sql_query)
         }
-
-        else if(folder == "physicalflows"){     
-            sql_query = "INSERT INTO physicalflows VALUES"
-            for (let i = 0; i < csv_json.length - 1; i++) {
-                let temp = "('" + csv_json[i]['DateTime'] + "','" + csv_json[i]['ResolutionCode'] +"','"+ csv_json[i]['FlowValue'] + "','"  + csv_json[i]['UpdateTime'] + "','" + csv_json[i]['InAreaName']  + "','" + csv_json[i]['OutAreaName']+ "')" 
-                if(i < csv_json.length - 2)
-                    temp += ",";
-                sql_query += temp;
-            }
-            sql_query += ";";
-            sql_query = sql_query.replaceAll("''", null);
+        catch{
+            res.status(400).send("Error3: either making query or writing file");
         }
-
-        else if(folder == "actualtotalload") {
-            sql_query = "INSERT INTO actualtotalload VALUES"
-            for (let i = 0; i < csv_json.length - 1; i++) {
-                let temp = "('" + csv_json[i]['DateTime'] + "','" + csv_json[i]['ResolutionCode'] +"','"+ csv_json[i]['TotalLoadValue'] + "','" + csv_json[i]['UpdateTime'] + "','" + csv_json[i]['AreaName'] + "')" 
-                if(i < csv_json.length - 2)
-                    temp += ",";
-                sql_query += temp;
-            }
-            sql_query += ";";
-        }
-
-        res.send(sql_query)
     }
     else{
         //send res error need parameters
