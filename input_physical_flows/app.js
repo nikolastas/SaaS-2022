d1692b9b054c2027700350ce7565bd0aebe88574
Nikolaos Tasiopoulos <nikolas2000tas@gmail.com>
2022-07-09 18:45:53 +0300

    refactor some files

--- input_physical_flows/app.js
+++ input_physical_flows/app.js
@@ -2,16 +2,15 @@ const express = require('express');
 // const cors = require('cors');
 // const auth = require('./utils/auth');
 
-const routes = require('./endpoints/routes.js');
-
+const request = require('./controller/request.js');
 
 const app = express();
 
 // app.use(cors());
 // app.use(auth);
+request.make_request();
 
 
-app.use('/', routes);
 
 
 module.exports = app;
--- input_physical_flows/utils/database.js
+++ input_physical_flows/utils/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 let con = mysql.createConnection({
     host: "localhost",
     user:"root",
-    password:"",
+    password:"root",
     database:"saas"
 });
 
