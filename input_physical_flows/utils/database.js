c67b88cbb089834f9952a5d9d924b20fb6157cfb
Orestiszar <zarasorestis@gmail.com>
2022-07-14 02:00:59 +0300

    changed from local to cloud sql databases

--- input_physical_flows/utils/database.js
+++ input_physical_flows/utils/database.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 let con = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"root",
-    database:"input_ff"
+    // host: "localhost",
+    // user:"root",
+    // password:"root",
+    // database:"input_ff"
+    host: "35.204.254.162",//needs to be changed to unix sock
+    user: "root",
+    password: "root",
+    database: "input_ff"
 });
 
 
